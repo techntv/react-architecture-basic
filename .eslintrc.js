c75d33ee2c02138326a399ba896bfe7d940a7093
The Vinh <v.vinhnt24@vinid.net>
2020-07-26 08:17:39 +0700

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+    ],
+    "rules": {
+    }
+};
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+import { Provider } from 'react-redux'
+import { configureStore } from './store'
+
+ReactDOM.render(<Provider store={configureStore()}>
+  <App />
+</Provider>, document.getElementById('root'));
